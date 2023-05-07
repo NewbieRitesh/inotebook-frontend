0407a8507be195817494ac142b26cb94307022ba
NewbieRitesh <newbieritesh@gmail.com>
2023-05-07 14:57:44 +0530

    updating login page functions

--- src/components/logs/Login.js
+++ src/components/logs/Login.js
@@ -1,66 +1,21 @@
 import React, { useContext, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import alertContext from '../../context/alert/alertContext';
+import userContext from '../../context/users/userContext';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [showPassword, setShowPassword] = useState({ show: false, inputType: "password", iconClassText: "fa-eye" })
+    // state declaration
     const [credentials, setCredentials] = useState({ email: "", password: "" })
-
-    const useAlertContext = useContext(alertContext)
+    // importing and destructing Contexts and hooks
+    const useUserContext = useContext(userContext)
+    const { userLogin, showPasswordFunc, showPassword } = useUserContext;
     const navigate = useNavigate();
 
-    const handleChange = async (event) => {
-        setCredentials({ ...credentials, [event.target.name]: event.target.value })
-    }
-
-    const showPasswordFunc = () => {
-        if (showPassword.show) {
-            setShowPassword({ show: false, inputType: "password", iconClassText: "fa-eye" })
-        } else {
-            setShowPassword({ show: true, inputType: "text", iconClassText: "fa-eye-slash" })
-        }
-    }
-
+    // handling user inputs and login click
+    const handleChange = async (event) => setCredentials({ ...credentials, [event.target.name]: event.target.value })
     const clickLogin = async (e) => {
         e.preventDefault()
-        let headersList = {
-            "Content-Type": "application/json"
-        }
-
-        let bodyContent = JSON.stringify({
-            "email": credentials.email,
-            "password": credentials.password
-        });
-
-        let response = await fetch("http://localhost:1000/api/auth/login", {
-            method: "POST",
-            body: bodyContent,
-            headers: headersList
-        });
-
-        let data = await response.json();
-        console.log(data);
-        if (response.status === 200) {
-            useAlertContext.showAlert("success", "You Logged in Successfully")
-        } else if (response.status === 400) {
-            if (data.success === false) {
-                useAlertContext.showAlert("error", data.error)
-            }
-            else {
-                let alertErrorString = ""
-                // useAlertContext.showAlert("error")
-                for (let index = 0; index < data.errors.length; index++) {
-                    console.log(data.errors[index].msg);
-                    alertErrorString += data.errors[index].msg + ". "
-                }
-                useAlertContext.showAlert("error", alertErrorString);
-            }
-        }
-        if (data.success) {
-            // save the auth token in local storage and redirect to home page
-            localStorage.setItem("token", data.authToken)
-            navigate('/')
-        }
+        const response = await userLogin(credentials.email, credentials.password)
+        if (response.success === true) navigate('/')
     }
 
     return (
@@ -76,7 +31,7 @@ const Login = () => {
                     <input type={`${showPassword.inputType}`} name='password' className="form-control" id="exampleInputPassword1" onChange={handleChange} />
                     <span style={{ cursor: "pointer", fontSize: "14px" }} onClick={showPasswordFunc}><i className={`fa-regular ${showPassword.iconClassText}`} />{showPassword.show ? " Hide Password" : " Show Password"}</span>
                 </div>
-                <button disabled={credentials.password.length < 6} type="submit" className="btn btn-primary" style={{ width: "100%" }} onClick={clickLogin}>LogIn</button>
+                <button disabled={credentials.password.length < 6} type="submit" className="btn btn-primary" style={{ width: "100%" }} >LogIn</button>
             </div>
         </form>
     )
--- src/context/users/UserState.js
+++ src/context/users/UserState.js
@@ -1,136 +1,154 @@
 import React, { useContext, useState } from 'react'
 import UserContext from './userContext'
 import alertContext from '../alert/alertContext'
+
 const UserState = (props) => {
+    // initilization of states
+    const [userData, setUserData] = useState([])
+    const [showPassword, setShowPassword] = useState({ show: false, inputType: "password", iconClassText: "fa-eye" })
+    // importing contexts
     const useAlertContext = useContext(alertContext);
     const { showAlert } = useAlertContext;
-    const [userData, setUserData] = useState([])
-    // get user data (name)
+
+
+    // show password function
+    const showPasswordFunc = () => {
+        if (showPassword.show) setShowPassword({ show: false, inputType: "password", iconClassText: "fa-eye" })
+        else setShowPassword({ show: true, inputType: "text", iconClassText: "fa-eye-slash" })
+    }
+
+    // function to call login api request 
+    const userLogin = async (email, password) => {
+        let headersList = {
+            "Content-Type": "application/json"
+        }
+        let bodyContent = JSON.stringify({
+            "email": email,
+            "password": password
+        });
+        let response = await fetch("http://localhost:1000/api/auth/login", {
+            method: "POST",
+            body: bodyContent,
+            headers: headersList
+        });
+        let data = await response.json();
+        if (data) useAlertContext.showAlert(data.success === true ? "success" : "error", data.response)
+        if (data.success) {
+            // save the auth token in local storage and redirect to home page
+            localStorage.setItem("token", data.authToken)
+        }
+        return data
+    }
+
+    // api call to get user data (name)
     const getUserData = async () => {
         let headersList = {
             "content-type": "application/json",
             "auth-token": localStorage.getItem('token')
         }
-
         let response = await fetch("http://localhost:1000/api/auth/getuser", {
             method: "POST",
             headers: headersList
         });
-
         let data = await response.json();
         setUserData(data)
-        // console.log(userData);
     }
 
-    // update user data (name)
+    // api call to update user data (name)
     const updateUserData = async (id, newName) => {
         let headersList = {
             "auth-token": localStorage.getItem('token'),
             "Content-Type": "application/json"
         }
-
         let bodyContent = JSON.stringify({
             "name": newName,
         });
-
         let response = await fetch(`http://localhost:1000/api/auth/update-user-data/${id}`, {
             method: "PUT",
             body: bodyContent,
             headers: headersList
         });
-
-        let data = await response.text();
-        console.log(data);
-        if (response.status === 200) {
-            showAlert("success", "Data Updated Successfully")
-        } else {
-            showAlert("error", "Some error occur")
-        }
+        // showing alert when data will be updated
+        if (response.status === 200) showAlert("success", "Data Updated Successfully")
+        else showAlert("error", "Some error occur")
     }
 
-    const updateUserEmail = async (id, email, authPassword) => {
-        let headersList = {
-            "auth-token": localStorage.getItem('token'),
-            "Content-Type": "application/json"
-        }
-
-        let bodyContent = JSON.stringify({
-            "email": email,
-            "authPassword": authPassword
-        });
-        let response = await fetch(`http://localhost:1000/api/auth/update-user-email/${id}`, {
-            method: "POST",
-            body: bodyContent,
-            headers: headersList
-        });
-
-        let data = await response.text();
-        return JSON.parse(data)
-    }
-
+    // api call to authenticate user
     const authenticateUser = async (authPassword) => {
         let headersList = {
             "auth-token": localStorage.getItem('token'),
             "Content-Type": "application/json"
         }
-
         let bodyContent = JSON.stringify({
             "password": authPassword
         });
-
         let response = await fetch("http://localhost:1000/api/auth/authenticate/6453a5f69f1ec36802f7554c", {
             method: "POST",
             body: bodyContent,
             headers: headersList
         });
-
         let data = await response.text();
         return JSON.parse(data)
     }
 
+    // api call to update user email
+    const updateUserEmail = async (id, email, authPassword) => {
+        let headersList = {
+            "auth-token": localStorage.getItem('token'),
+            "Content-Type": "application/json"
+        }
+        let bodyContent = JSON.stringify({
+            "email": email,
+            "authPassword": authPassword
+        });
+        let response = await fetch(`http://localhost:1000/api/auth/update-user-email/${id}`, {
+            method: "PUT",
+            body: bodyContent,
+            headers: headersList
+        });
+        let data = await response.text();
+        return JSON.parse(data)
+    }
+
+    // api call to update user password
     const updateUserPassword = async (id, newPassword, authPassword) => {
         let headersList = {
             "auth-token": localStorage.getItem('token'),
             "Content-Type": "application/json"
         }
-
         let bodyContent = JSON.stringify({
             "newPassword": newPassword,
             "authPassword": authPassword
-        }
-        );
-
+        });
         let response = await fetch(`http://localhost:1000/api/auth/update-user-password/${id}`, {
             method: "PUT",
             body: bodyContent,
             headers: headersList
         });
-
         let data = await response.text();
         return JSON.parse(data)
     }
 
-    const deleteUser = async (id, authPassword)=> {
+    // api call to delete user 
+    const deleteUser = async (id, authPassword) => {
         let headersList = {
             "auth-token": localStorage.getItem('token'),
             "Content-Type": "application/json"
         }
-
         let bodyContent = JSON.stringify({
             "authPassword": authPassword
         });
-
-        let response = await fetch(`http://localhost:1000/api/auth/delete-user/${id}` , {
+        let response = await fetch(`http://localhost:1000/api/auth/delete-user/${id}`, {
             method: "DELETE",
             body: bodyContent,
             headers: headersList
         })
         let data = await response.text();
-        return data
+        return JSON.parse(data)
     }
 
     return (
-        <UserContext.Provider value={{ getUserData, updateUserData, userData, updateUserPassword, setUserData, updateUserEmail, authenticateUser, deleteUser }}>
+        <UserContext.Provider value={{ userLogin, getUserData, showPasswordFunc, showPassword, updateUserData, userData, updateUserPassword, setUserData, updateUserEmail, authenticateUser, deleteUser }}>
             {props.children}
         </UserContext.Provider>
     )
