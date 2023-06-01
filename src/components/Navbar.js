2be68ae5b26fca714cbaf866d90b1f1d5b1078d1
NewbieRitesh <newbieritesh@gmail.com>
2023-06-01 15:41:41 +0530

    fixing navbar toggle close

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,46 @@
 import React from 'react'
+import { useRef } from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  const toggleClose = useRef()
   const navigate = useNavigate()
   const handleLogout = () => {
     localStorage.removeItem('token');
     navigate('/login')
   }
+  const closeNavbar = () => {
+    toggleClose.current.click()
+  }
   return (
     <><nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark"><div className="container-fluid">
       <Link className="navbar-brand" to="/">iNotebook</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button ref={toggleClose} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link className={`nav-link ${useLocation().pathname === '/' ? 'active' : ''}`} aria-current="page" to="/" >Home</Link>
+            <Link className={`nav-link ${useLocation().pathname === '/' ? 'active' : ''}`} aria-current="page" to="/" onClick={closeNavbar}>Home</Link>
           </li>
           <li className="nav-item">
-            <Link className={`nav-link ${useLocation().pathname === '/about' ? 'active' : ''}`} to="/about">about</Link>
+            <Link className={`nav-link ${useLocation().pathname === '/about' ? 'active' : ''}`} to="/about" onClick={closeNavbar}>about</Link>
           </li>
         </ul>
         <div className="d-flex">
-          {localStorage.getItem('token') ?<><Link className='btn' to="/profile">Profile</Link> <button className="btn btn-primary mx-1" onClick={handleLogout}>Logout</button></> : <><Link className="btn btn-primary mx-1" to="/login">Login</Link><Link className="btn btn-outline-primary mx-1" to="/signup">Sign Up</Link></>}
+          {localStorage.getItem('token') ?
+            <>
+              <Link className='btn btn-outline-primary text-white ' to="/profile" onClick={closeNavbar}>Profile</Link>
+              <button className="btn btn-primary mx-1" onClick={() => {
+                handleLogout()
+                closeNavbar()
+              }}>Logout</button>
+            </> :
+            <>
+              <Link className="btn btn-primary mx-1" to="/login" onClick={closeNavbar}>Login</Link>
+              <Link className="btn btn-outline-primary mx-1" to="/signup" onClick={closeNavbar}>Sign Up</Link>
+            </>
+          }
         </div>
       </div>
     </div></nav></>
